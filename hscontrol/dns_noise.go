c8251d170f4ce3646def58940ffaff7a98af0648
Viet Pham <viet@webonyx.com>
2025-02-13 12:59:43 +0700

    Cloudflare works

--- hscontrol/dns_noise.go
+++ hscontrol/dns_noise.go
@@ -12,12 +12,17 @@ import (
 	"net/http"
 	"strings"
 	"tailscale.com/tailcfg"
+	"time"
 )
 
-var provider interface {
+var dnsProvider interface {
 	libdns.RecordGetter
 	libdns.RecordAppender
 	libdns.RecordSetter
+}
+
+func init() {
+	viper.SetDefault("acme.dns.propagation_timeout", "10s")
 }
 
 func (ns *noiseServer) NoiseSetDnsHandler(
@@ -47,60 +52,51 @@ func (ns *noiseServer) NoiseSetDnsHandler(
 		Msg("SetDNSHandler called")
 
 	ctx := req.Context()
-	domain := viper.GetString("dns.base_domain")
-	zone := domain
-	txt := setDnsRequest.Value
+	baseDomain := viper.GetString("dns.base_domain")
+	dnsZone := subdomainToDomain(baseDomain)
 
-	if viper.GetString("duckdns.api_token") != "" {
-		provider = &duckdns.Provider{APIToken: viper.GetString("duckdns.api_token")}
-	}
-
-	if viper.GetString("cloudflare.api_token") != "" {
-		zone = subdomainToDomain(domain)
-		txt = fmt.Sprintf("\"%s\"", setDnsRequest.Value)
-		provider = &cloudflare.Provider{APIToken: viper.GetString("cloudflare.api_token")}
-	}
-
-	if provider == nil {
+	apiToken := viper.GetString("acme.dns.api_token")
+	if apiToken == "" {
 		log.Error().
 			Caller().
-			Msg("no dns provider setup")
-		http.Error(writer, "Internal error", http.StatusInternalServerError)
-		return
-	}
-
-	records := []libdns.Record{
-		{
-			Type:  setDnsRequest.Type,
-			Name:  setDnsRequest.Name,
-			Value: txt,
-		},
-	}
-
-	// list records
-	recs, err := provider.GetRecords(ctx, zone)
-	if err != nil {
-		log.Error().
-			Caller().
-			Err(err).
-			Msg("Could not retrieve dns records for: " + domain)
+			Msg("Missing HEADSCALE_ACME_DNS_API_TOKEN")
 		http.Error(writer, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
-	var recordExists = false
-	relativeName := libdns.RelativeName(setDnsRequest.Name, zone)
-	for _, re := range recs {
-		if re.Name == relativeName {
-			recordExists = true
-		}
-
-		log.Info().Msg(fmt.Sprintf("%s %s", re.Type, re.Name))
+	if strings.HasSuffix(baseDomain, "duckdns.org") {
+		dnsZone = baseDomain
+		dnsProvider = &duckdns.Provider{APIToken: apiToken}
+	} else {
+		dnsProvider = &cloudflare.Provider{APIToken: apiToken}
 	}
 
-	if recordExists {
-		log.Info().Msg(fmt.Sprintf("Record existed, overridding: %s", setDnsRequest.Name))
-		setRecs, err := provider.SetRecords(ctx, zone, records)
+	// list records
+	recs, err := dnsProvider.GetRecords(ctx, dnsZone)
+	if err != nil {
+		log.Error().
+			Caller().
+			Err(err).
+			Msg("Could not retrieve dns records for zone: " + baseDomain)
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	var existingRecord *libdns.Record
+	relativeName := libdns.RelativeName(setDnsRequest.Name, dnsZone)
+
+	for _, re := range recs {
+		if re.Name == relativeName {
+			existingRecord = &re
+		}
+
+		//log.Info().Msg(fmt.Sprintf("%s %s", re.Type, re.Name))
+	}
+
+	if existingRecord != nil {
+		log.Info().Msg(fmt.Sprintf("TXT record already existed, updating: %s", setDnsRequest.Name))
+		existingRecord.Value = setDnsRequest.Value
+		setRecs, err := dnsProvider.SetRecords(ctx, dnsZone, []libdns.Record{*existingRecord})
 		if err != nil {
 			log.Error().
 				Caller().
@@ -110,11 +106,16 @@ func (ns *noiseServer) NoiseSetDnsHandler(
 			return
 		}
 		for _, re := range setRecs {
-			log.Info().Msg(fmt.Sprintf("Updated record: %s", re.Name))
+			log.Info().Msg(fmt.Sprintf("Did updated: %s -> %s", re.Name, re.Value))
 		}
-
 	} else {
-		newRecs, err := provider.AppendRecords(ctx, zone, records)
+		newRecs, err := dnsProvider.AppendRecords(ctx, dnsZone, []libdns.Record{
+			{
+				Type:  setDnsRequest.Type,
+				Name:  setDnsRequest.Name,
+				Value: setDnsRequest.Value,
+			},
+		})
 		if err != nil {
 			log.Error().
 				Caller().
@@ -125,13 +126,18 @@ func (ns *noiseServer) NoiseSetDnsHandler(
 		}
 
 		for _, re := range newRecs {
-			log.Info().Msg(fmt.Sprintf("Added new %s record set: %s", re.Type, re.Name))
+			log.Info().Msg(fmt.Sprintf("Added TXT record %s, Value %s", re.Name, re.Value))
 		}
 	}
+
+	// Give Cloudflare enough time for dns propagation
+	propagationTimeout := viper.GetDuration("acme.dns.propagation_timeout")
+	time.Sleep(propagationTimeout)
 
 	resp := tailcfg.SetDNSResponse{}
 	respBody, _ := json.Marshal(resp)
 	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	writer.WriteHeader(200)
 	_, err = writer.Write(respBody)
 	if err != nil {
 		log.Error().
